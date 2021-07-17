c9c6a2e252ce5a3a22570606005986372d4f5c82
arfrhmnshrtn <iw8628940@gmail.com>
2021-07-17 17:37:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('.header');
+const bars = header.querySelector('.bars');
+const nav = document.querySelector('.header .list-menu');
+const navBar = document.querySelector('.header .bars i')
+window.addEventListener("scroll", function(event) {
+    if(this.scrollY > 20){
+        header.classList.add("sticky");
+    }else{
+        header.classList.remove("sticky");
+    }
+  
+});
+
+bars.addEventListener('click', function(){
+    nav.classList.toggle('active');
+    navBar.classList.toggle('active');
+});
