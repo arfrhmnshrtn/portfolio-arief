3029e19c7d8ebff54c394207ff1de3ae44635fe2
arfrhmnshrtn <iw8628940@gmail.com>
2021-07-24 13:56:58 +0700

    menambahkan efek animasi

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const header = document.querySelector('.header');
 const bars = header.querySelector('.bars');
 const nav = document.querySelector('.header .list-menu');
-const navBar = document.querySelector('.header .bars i')
+const navBar = document.querySelector('.header .bars i');
+const cardAbout = document.querySelectorAll('#about .card');
+const btnCardAbout = document.querySelectorAll('#about .card i');
+const textHidden = document.querySelector('#about .card .hidden');
+console.log(btnCardAbout)
+
+
 window.addEventListener("scroll", function(event) {
     if(this.scrollY > 50){
         header.classList.add("sticky");
@@ -11,8 +17,16 @@ window.addEventListener("scroll", function(event) {
   
 });
 
-bars.addEventListener('click', function(){
-    nav.classList.toggle('active');
-    navBar.classList.toggle('active');
+btnCardAbout.forEach(el => {
+    el.addEventListener('click', function(e){
+        e.target.classList.add('timbul');
+        console.log(e.target);
+    });
 });
 
+let typed = new Typed('.profesi', {
+    strings: ["Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
